42d73a90a0de99602e7db7e0c18f61931a8c7f0f
Neola Dewhurst <ndewhurst@silvercloudinc.com>
2018-09-14 13:49:12 -0400

    Initialize repository

---
+++ lib/ndewhurst_palindrome/version.rb
@@ -0,0 +1,3 @@
+module NdewhurstPalindrome
+  VERSION = "0.1.0"
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+require "ndewhurst_palindrome"
+
+require "minitest/autorun"
+require "minitest/reporters"
+Minitest::Reporters.use!
